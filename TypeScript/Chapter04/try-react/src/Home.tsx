ecea77f231145a12c038731533323fa35747d08f
timmyfight <gtaaa@wp.pl>
2023-01-08 20:22:51 +0100

    Creat react app, Hello world in main view and add routing and second view

---
+++ TypeScript/Chapter04/try-react/src/Home.tsx
@@ -0,0 +1,7 @@
+import {FC} from "react";
+
+const Home: FC = () => {
+  return <div>Welcome World!</div>;
+}
+
+export default Home;
---
+++ TypeScript/Chapter04/try-react/src/SecondScreen.tsx
@@ -0,0 +1,7 @@
+import {FC} from "react";
+
+const SecondScreen: FC = () => {
+  return <div>Second screen!</div>;
+}
+
+export default SecondScreen;
