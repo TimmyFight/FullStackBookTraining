4f878c476c032d4a895c842552119debdfee2dec
timmyfight <gtaaa@wp.pl>
2023-02-01 17:16:12 +0100

    Using POST method

--- TypeScript/Chapter08/node-server/server.mjs
+++ TypeScript/Chapter08/node-server/server.mjs
@@ -1,8 +1,25 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.end("Welcome World!");
+  if (req.url === "/") {
+    res.end("Welcome World!");
+  } else if (req.url === "/a") {
+    res.end("Welcome on the route A!");
+  } else if (req.url === "/b") {
+    res.end("Welcome on the route B!");
+  } else if (req.url === "/c" && req.method === "POST") {
+    let body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const params = Buffer.concat(body);
+      console.log("Content: ", params.toString());
+      res.end(`Sended parameters: ${params.toString()}`);
+    });
+  } else {
+    res.end("See you soon!");
+  }
 });
 
 const port = 8000;
